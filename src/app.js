18c6b359f1008e4397dd624e53162ea52538fd86
Yanbeckford <yanbeckford@gmail.com>
2023-10-04 19:39:23 +0100

    First push

---
+++ src/app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const db = require("./config/db")
+const users = require("./routes/user")
+const products = require("./routes/products")
+const category = require("./routes/category")
+const comments = require("./routes/comments")
+const contact = require("./routes/contact")
+
+
+
+const port = 5000;
+
+app.use(express.json)
+app.use(express.urlencoded({extended:true}))
+app.use("/api/users",users)
+app.use("/api/products",products)
+app.use("/api/category",category)
+app.use("/api/comment",comments)
+app.use("/api/contact",contact)
+
+
+
+
+
+  app.listen(port, () => { 
+  console.log(`Server started on port ${port}` );
+  })
