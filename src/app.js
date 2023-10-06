648867456a7fe0a61f9b84754e1e068c098620d0
Derrick <mereo83@gmail.com>
2023-10-07 02:33:12 +0400

    updated app.js with session,cookie parser

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
+const cookieParser = require('cookie-parser');
+const session = require('express-session')
 const db = require("./config/db")
 const users = require("./routes/user")
 const products = require("./routes/products")
@@ -13,6 +15,20 @@ const port = 5000;
 
 app.use(express.json)
 app.use(express.urlencoded({extended:true}))
+
+app.use(cookieParser());
+
+app.use(
+  session({
+    secret: 'eye_color', 
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 3600000, // 1 hour (in milliseconds)
+    },
+  })
+);
+
 app.use("/api/users",users)
 app.use("/api/products",products)
 app.use("/api/category",category)
